201482ed9facc242ef622a4489a53b8e8a34d0dc
Powers <powertle@outlook.com>
2023-10-20 14:36:18 +0800

    add web

---
+++ src/HandJob.Web/Vars/GlobalVars.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HandJob.Web.Vars
+{
+    public class GlobalVars
+    {
+        public const string AppName = "冲了么App";
+
+        public const string DoJob = "戒";
+        public const string UndoJob = "冲";
+    }
+}
